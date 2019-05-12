72bffd509d102c10c45555721409ac6f2037dbdb
libin1991 <3120217729@qq.com>
2019-05-12 15:56:38 +0800

    rp webpack config

--- config-overrides.js
+++ config-overrides.js
@@ -25,7 +25,8 @@ const addCustomize = () => config => {
                 viewportUnit: 'vw', // (String) Expected units.
                 selectorBlackList: ['.ignore', '.hairlines'], // (Array) The selectors to ignore and leave as px.
                 minPixelValue: 1, // (Number) Set the minimum pixel value to replace.
-                mediaQuery: false // (Boolean) Allow px to be converted in media queries.
+                mediaQuery: false, // (Boolean) Allow px to be converted in media queries.
+                exclude:/heart/gi
             }),
             require('postcss-write-svg')({
                 utf8: false
